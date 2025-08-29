975434648c96fdbea73cc6fe0bee5ef0ef3276c8
RatanGaneshAmin <110844670+RatanGaneshAmin@users.noreply.github.com>
2025-08-29 11:21:33 +0530

    Update and rename index.js to bfhl.js

---
+++ api/bfhl.js
@@ -0,0 +1,53 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const data = req.body.data || [];
+
+      let odd_numbers = [];
+      let even_numbers = [];
+      let alphabets = [];
+      let special_characters = [];
+      let sum = 0;
+
+      data.forEach(item => {
+        if (/^-?\d+$/.test(item)) {
+          let num = parseInt(item, 10);
+          sum += num;
+          if (num % 2 === 0) even_numbers.push(item);
+          else odd_numbers.push(item);
+        } else if (/^[a-zA-Z]+$/.test(item)) {
+          alphabets.push(item.toUpperCase());
+        } else {
+          special_characters.push(item);
+        }
+      });
+
+      let allAlpha = alphabets.join("");
+      let reversed = allAlpha.split("").reverse().join("");
+      let concat_string = reversed
+        .split("")
+        .map((ch, i) => (i % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase()))
+        .join("");
+
+      res.status(200).json({
+        is_success: true,
+        user_id: "ratan_amin_17012005",
+        email: "ratangamin@gmail.com",
+        roll_number: "22BCE3692",
+        odd_numbers,
+        even_numbers,
+        alphabets,
+        special_characters,
+        sum: sum.toString(),
+        concat_string
+      });
+    } catch (error) {
+      res.status(500).json({
+        is_success: false,
+        message: error.message
+      });
+    }
+  } else {
+    res.status(405).json({ is_success: false, message: "Method not allowed" });
+  }
+}
